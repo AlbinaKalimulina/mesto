399693cfec065e4180818d0c9effee55d43902c2
Albina Kalimulina <albinashamig@yandex.ru>
2023-04-13 22:57:41 +0500

    ПР4

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,19 @@
 const openPopupButton = document.querySelector('.profile__edit-button_popup-open');
 const popup = document.querySelector('.popup');
+const popupCloseButton = document.querySelector('.popup__close-button');
 
-function togglePopup() {
-  popup.classList.toggle('popup_opened');
+function openPopup() {
+  popup.classList.add('popup_opened');
 }
 
-openPopupButton.addEventListener('click', togglePopup);
+function closePopup(evt) {
+  const isOverlay = evt.target.classList.contains('popup');
+  const isCloseButton = evt.target.classList.contains('popup__close-button');
+
+  if (isOverlay || isCloseButton) {
+    popup.classList.remove('popup_opened');
+  }
+}
+
+openPopupButton.addEventListener('click', openPopup);
+popup.addEventListener('click', closePopup);
